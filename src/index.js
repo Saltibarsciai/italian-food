f9577e62723823f2f254a7b11a54ef4971a87230
Softelicious <dominykas.rakunas@gmail.com>
2019-11-16 18:54:56 +0200

    separated templates and mobile style from main scss files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,4 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<RootApp />, document.getElementById('root'));
 
-// If you want your root-app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
