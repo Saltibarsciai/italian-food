cbe55ce73f80949f97918bf94b4ba5d17689f65a
Softelicious <dominykas.rakunas@gmail.com>
2019-11-16 21:33:03 +0200

    remove bad font

--- src/root-app/root-app.js
+++ src/root-app/root-app.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import style from './root-app.module.scss'
 import LandingPage from "../components/landing-page/landing-page";
-
 
 const RootApp = () => {
     return (
-        <div className={style.app}>
+        <>
             <LandingPage/>
-        </div>
+        </>
     );
 };
 
