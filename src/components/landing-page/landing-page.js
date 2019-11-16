69fe8f45745f12456dc034ce194a5ebd5b58c76e
Softelicious <dominykas.rakunas@gmail.com>
2019-11-16 16:36:03 +0200

    name and create grid areas

--- src/components/landing-page/landing-page.js
+++ src/components/landing-page/landing-page.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import style from './landing-page.module.scss'
 import TopView from "./top-view/top-view";
 import MiddleView from "./middle-view/middle-view";
 import BottomView from "./bottom-view/bottom-view";
 
 const LandingPage = () => {
     return (
-        <div className={style.container}>
+        <>
             <TopView/>
             <MiddleView/>
             <BottomView/>
-        </div>
+        </>
     );
 };
 
